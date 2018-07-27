c2bb46e1701cd30d4b9a0240af981e08a4fffb1c
Niklas Hanft <hanft@newsaktuell.de>
2018-07-27 14:29:24 +0200

    Improve build output & performance

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
+var webpack = require('webpack');
+
 module.exports = {
+  mode: 'production',
   entry: "./build/index.js",
   output: {
     filename: "bundle.js",
     path: __dirname + "/dist",
     library: 'nanewsformat'
   },
-
-  // Enable sourcemaps for debugging webpack's output.
+  optimization: {
+    minimize: true,
+    mergeDuplicateChunks: true
+  },
   devtool: "source-map",
-
   resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
     extensions: [".js"]
   },
-
   module: {
     rules: [
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
     ]
-  },
+  }
 };
