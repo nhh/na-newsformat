322cd7b2cc4e048bf385f63a966eab1c36441ee3
Niklas Hanft <hanft@newsaktuell.de>
2018-07-27 14:05:54 +0200

    Configure library name and downgrade webpack version

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ module.exports = {
   entry: "./build/index.js",
   output: {
     filename: "bundle.js",
-    path: __dirname + "/dist"
+    path: __dirname + "/dist",
+    library: 'nanewsformat'
   },
 
   // Enable sourcemaps for debugging webpack's output.
